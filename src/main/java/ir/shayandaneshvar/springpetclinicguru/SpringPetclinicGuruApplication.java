137608c8c2192671ef48e09bf6cf5e5d6926c913
shayandaneshvar1 <daneshvarshayan1@gmail.com>
2019-08-27 20:36:08 +0430

    Initiated Spring

---
+++ src/main/java/ir/shayandaneshvar/springpetclinicguru/SpringPetclinicGuruApplication.java
@@ -0,0 +1,13 @@
+package ir.shayandaneshvar.springpetclinicguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetclinicGuruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/shayandaneshvar/springpetclinicguru/SpringPetclinicGuruApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.shayandaneshvar.springpetclinicguru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPetclinicGuruApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
